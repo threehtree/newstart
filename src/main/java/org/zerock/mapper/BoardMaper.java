c581e222cf310b3fbf10d53d09f8ca9a0b72d3ba
threehtree <100838926+threehtree@users.noreply.github.com>
2022-04-29 16:15:22 +0900

    Board two Param Test [Parameter [arg1, arg0, param1, param2] OK

--- src/main/java/org/zerock/mapper/BoardMaper.java
+++ src/main/java/org/zerock/mapper/BoardMaper.java
@@ -1,5 +1,6 @@
 package org.zerock.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.zerock.domain.Board;
 
 import java.util.List;
@@ -7,7 +8,7 @@ import java.util.List;
 public interface BoardMaper {
     //Mapper 작성하기전에 기준점을 잡아 두기위해 domain 을 먼저 설정한다
     void insert(Board board);
-    List<Board> selectList (int num, int d);// 일단은 빈 생성자
+    List<Board> selectList (@Param("skip") int num,@Param("size") int d);// 일단은 빈 생성자
 
 
 }
