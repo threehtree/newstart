467c3cff5cb40fd79af48f1665d953ad45f0ff59
threehtree <100838926+threehtree@users.noreply.github.com>
2022-04-29 15:30:35 +0900

    mapper TEST Setting

---
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,20 @@
+package org.zerock.mapper;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations="file:src/main/webapp/WEB-INF/root-context.xml")
+public class BoardMapperTests {
+   @Autowired(required = false)
+   private BoardMaper boardMaper; //BoardMapper의존성이 필요하다
+
+
+
+
+}
