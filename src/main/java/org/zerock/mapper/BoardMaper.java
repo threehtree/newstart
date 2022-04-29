2a272786b6db630a57c2bbce9935b5143862e058
threehtree <100838926+threehtree@users.noreply.github.com>
2022-04-29 16:49:05 +0900

    Board Insert Last index return

--- src/main/java/org/zerock/mapper/BoardMaper.java
+++ src/main/java/org/zerock/mapper/BoardMaper.java
@@ -7,7 +7,8 @@ import java.util.List;
 
 public interface BoardMaper {
     //Mapper 작성하기전에 기준점을 잡아 두기위해 domain 을 먼저 설정한다
-    void insert(Board board);
+    Integer insert(Board board);
+    //SelectKey를 이용해서 마지막 index값 을 반환
     List<Board> selectList (@Param("skip") int num,@Param("size") int d);// 일단은 빈 생성자
 
 
