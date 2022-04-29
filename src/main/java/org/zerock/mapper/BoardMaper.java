948b3414b05973dc394d22372231720fa188e8c6
threehtree <100838926+threehtree@users.noreply.github.com>
2022-04-29 15:46:13 +0900

    Board SelectList mapper

--- src/main/java/org/zerock/mapper/BoardMaper.java
+++ src/main/java/org/zerock/mapper/BoardMaper.java
@@ -2,9 +2,12 @@ package org.zerock.mapper;
 
 import org.zerock.domain.Board;
 
+import java.util.List;
+
 public interface BoardMaper {
     //Mapper 작성하기전에 기준점을 잡아 두기위해 domain 을 먼저 설정한다
     void insert(Board board);
+    List<Board> selectList ();// 일단은 빈 생성자
 
 
 }
